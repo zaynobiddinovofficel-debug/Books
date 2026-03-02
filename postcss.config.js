964a2265ea574b9a7795be664f7c57a796b72f40
biloliddin <zaynobiddinovofficel@gmail.com>
2026-03-02 09:37:52 -1100

    Innitial Commit

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,4 @@ export default {
     autoprefixer: {},
   },
 }
+// bu yerda esa postcss.config.js fayli yaratiladi, bu faylda Tailwind CSS va Autoprefixer plaginlari konfiguratsiya qilinadi. Tailwind CSS plaginini qo'shish orqali Tailwind CSS frameworkidan foydalanish mumkin bo'ladi, Autoprefixer plaginini qo'shish orqali esa CSS kodiga avtomatik ravishda vendor prefixlar qo'shiladi, bu esa brauzerlararo moslikni yaxshilaydi. Bu konfiguratsiya fayli PostCSS tomonidan ishlatiladi, bu esa CSS kodini optimallashtirish va transformatsiya qilish uchun kerak bo'ladi.
